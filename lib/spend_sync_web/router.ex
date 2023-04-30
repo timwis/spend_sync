e12c54f68c7f6021d443c0e1c54014d93b429d6a
timwis <tim@timwis.com>
2023-04-30 13:07:02 +0100

    Add *temporary* BankConnectionController for easy editing during development

--- lib/spend_sync_web/router.ex
+++ lib/spend_sync_web/router.ex
@@ -18,6 +18,7 @@ defmodule SpendSyncWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    resources "/bank_connections", BankConnectionController
   end
 
   # Other scopes may use custom stacks.
