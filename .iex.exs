a102cac76e583c0d62395819f6c59b8101e6a069
timwis <tim@timwis.com>
2023-05-27 13:37:32 +0100

    Add IEX config

---
+++ .iex.exs
@@ -0,0 +1,17 @@
+alias SpendSync.{
+  Repo,
+  Plans,
+  Sync,
+  TransferLogs,
+  UserAccounts
+}
+
+alias SpendSync.Plans.{
+  BankAccount,
+  BankConnection,
+  Mandate,
+  Plan
+}
+
+alias SpendSync.TransferLogs.TransferLog
+alias SpendSync.UserAccounts.User
